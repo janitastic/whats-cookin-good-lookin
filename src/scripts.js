eb8beffe90e638071e511aac6ba233af1eab9dda
Jani Palomino <palomino.jani@gmail.com>
2021-12-05 13:28:22 -0700

    Update icon images to be white

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,6 @@
 import './styles.css';
 // import './css/index.scss';
+import './images/recipe-book.png';
 import './images/baking.png';
 import './images/like.png';
 import apiCalls from './apiCalls';
