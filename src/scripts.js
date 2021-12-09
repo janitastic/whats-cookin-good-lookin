3cee8edf8f977249874f5eb690fa09a0ef4b08a8
Jani Palomino <palomino.jani@gmail.com>
2021-12-09 16:51:30 -0700

    Create function for recipe card innerHTML

--- src/scripts.js
+++ src/scripts.js
@@ -11,22 +11,54 @@ import RecipeRepository from './classes/RecipeRepository';
 import recipeData from './data/recipes';
 import ingredientsData from './data/ingredients';
 
-
+//Query Selectors
 let recipeCardSection = document.getElementById('recipeCardSection');
 const allRecipesBtn = document.getElementById('recipesBtn');
-// window.addEventListener('load', displayAllRecipes);
+// const recipeCard = document.getElementById('${recipe.id}');
+
+//Event Listeners
+window.addEventListener('load', displayAllRecipes);
+recipeCardSection.addEventListener('click', displayFullRecipe);
 
 function displayAllRecipes() {
   recipeCardSection.innerHTML = '';
   recipeData.forEach(recipe => {
     return recipeCardSection.innerHTML +=
-    `<article class="card">
-    <div class="card-icons">
-    <img class="icon" src="images/like.png">
-    <img class="icon" src="images/baking.png">
-    </div>
-    <h3>${recipe.name}</h3>
-    <img class="thumbnail-image" src=${recipe.image}>
+    `<article class="card" id="${recipe.id}">
+      <div class="card-icons">
+      <img class="icon" src="images/like.png">
+      <img class="icon" src="images/baking.png">
+      </div>
+      <h3>${recipe.name}</h3>
+      <img class="thumbnail-image" src=${recipe.image}>
     </article>`
   });
+}
+
+function displayFullRecipe() {
+  recipeCardSection.innerHTML = '';
+  let recipeID = Number(event.target.parentNode.id);
+  for(var i = 0; i < recipeData.length; i++) {
+    if (recipeData[i].id === recipeID) {
+      recipeCardSection.innerHTML +=
+      `<article class="full-recipe">
+        <h3>Recipe Name</h3>
+        <img class="thumbnail-image" src="https://spoonacular.com/recipeImages/595736-556x370.jpg">
+          <h4>Ingredients</h4>
+            <ul>
+              <li>2 oz Ingredient Name</li>
+              <li>2 oz Ingredient Name</li>
+              <li>2 oz Ingredient Name</li>
+            </ul>
+          <h4>Directions</h4>
+            <ol>
+              <li>1. sdfjsdlkfjsdljkfs</li>
+              <li>1. sdfjsdlkfjsdljkfs</li>
+              <li>1. sdfjsdlkfjsdljkfs</li>
+              <li>1. sdfjsdlkfjsdljkfs</li>
+            </ol>
+          <h4>Total Cost = $27</h4>
+      </article>`
+    }
+  }
 }
