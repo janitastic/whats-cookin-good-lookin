fa56c420e9cebb954d740f15b9a55eb7683b290d
Christine Rowland <fordo29@gmail.com>
2021-12-09 15:59:50 -0700

    Refactor for loop to forEach

--- src/scripts.js
+++ src/scripts.js
@@ -17,20 +17,16 @@ const allRecipesBtn = document.getElementById('recipesBtn');
 window.addEventListener('load', displayAllRecipes);
 
 function displayAllRecipes() {
-  console.log(recipeData[0].name);
   recipeCardSection.innerHTML = '';
-  for(let i = 0; i < recipeData.length; i++) {
-    recipeCardSection.innerHTML +=
+  recipeData.forEach(recipe => {
+    return recipeCardSection.innerHTML +=
     `<article class="card">
     <div class="card-icons">
     <img class="icon" src="images/like.png">
     <img class="icon" src="images/baking.png">
     </div>
-    <h3>${recipeData[i].name}</h3>
-    <img class="thumbnail-image" src=${recipeData[i].image}>
+    <h3>${recipe.name}</h3>
+    <img class="thumbnail-image" src=${recipe.image}>
     </article>`
-  }
-};
-
-//import the recipes.js and ingredients.js
-// display all the cards in the DOM.
+  });
+}
