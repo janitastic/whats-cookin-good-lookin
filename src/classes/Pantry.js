b90592283a0fdc22590a6843d1a57b1d25036bb4
Emma Chory <emmachory@gmail.com>
2022-01-06 21:02:30 -0700

    Start compareIngredients method

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -1,17 +1,42 @@
 class Pantry  {
-  constructor(usersData) {
-    this.userName = usersData.name;
-    this.pantry = usersData.pantry
+  constructor(currentUser) {
+    this.user = currentUser;
+    this.pantry = currentUser.pantry
   }
 
-  findIngredients(recipe) {
+  compareIngredients(recipe) {
+    // checking to see what's in the pantry (ingredients)
+    // checking to see what ingredients are in the recipe
+    // checking to see if the pantry ingredients are enough for the recipe
+
+    const matchingIngredients = recipe.ingredients.every(recipeIngredient => {
+      let inPantry = this.pantry.some(pantryIngredient => {
+        pantryIngredient === recipeIngredient.id
+      });
+      let enoughIngredients;
+
+    const foundIngredient = this.pantry.find(pantryIngredient => {
+      ingredient.id === this.pantry.ingredient
+    })
+
+    
+      //I want this one to find a match - ingredient.id === this.pantry.ingredient
+    //   ingredient.quantity.amount >= this.pantry.
+    })
+    return result;
+    // const result = this.pantry.find((pantryIngredient) => {
+    //   pantryIngredient.ingredient === ingredient.id
+    //   // recipeData[0].ingredients.id === myPantry.pantry.ingredient
+    // })
     // for each ingredient in recipe
-    // find ingredient in this.ingredientsInPantry
+    // find ingredient in this.pantry
     // if it exists and the quantity is enough - good
-    // if it exists and the quantity is too low - show error
-    // if it doesn't exist - show error
+    
   }
 }
+
+// if it exists and the quantity is too low - show error
+// if it doesn't exist - show error
 
 
 
--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -3,14 +3,18 @@ import { expect } from 'chai';
 import User from '../src/classes/User';
 import Recipe from '../src/classes/Recipe';
 import usersData from '../src/data/users';
+import recipeData from '../src/data/recipes';
 
 describe('Pantry', () => {
-  let user;
-  let myPantry;
+  let user, myPantry, recipe1, recipe2, recipe3, recipe4;
    
   beforeEach(()=> {
     user = new User(usersData[0]);
     myPantry = new Pantry(user);
+    recipe1 = new Recipe(recipeData[0]);
+    recipe2 = new Recipe(recipeData[1]);
+    recipe3 = new Recipe(recipeData[2]);
+    recipe4 = new Recipe(recipeData[3]);
   })
 
   it('should instantiate a new pantry', () => {
@@ -21,7 +25,15 @@ describe('Pantry', () => {
     expect(myPantry).to.be.an.instanceof(Pantry);
   });
   
+  it('should have the correct amount of pantry items', () => {
+    expect(myPantry.pantry.length).to.equal(35);
+  })
 
+  it('should have enough ingredients to make a recipe', () => {
+    console.log(recipe1.ingredients)
+    console.log(myPantry.pantry)
+    expect(myPantry.compareIngredients(recipe2)).to.equal(true);
+  })
 }); 
 
 
